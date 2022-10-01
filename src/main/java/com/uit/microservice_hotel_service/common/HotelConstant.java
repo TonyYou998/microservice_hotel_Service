57b1dccb1542d6b4d211e32e505fab8af4002cbc
Tan Vuu <tanvuu@kms.com.vn>
2022-10-01 23:30:06 +0700

    change something

--- src/main/java/com/uit/microservice_hotel_service/common/HotelConstant.java
+++ src/main/java/com/uit/microservice_hotel_service/common/HotelConstant.java
@@ -4,18 +4,19 @@ import com.uit.microservice_base_project.common.BaseConstant;
 
 public class HotelConstant extends BaseConstant {
 
-    public static final String demo="/host/demo";
-    public static final String create_a_room="create/create-a-room";
+    public static final String demo="/demo";
+    public static final String create_a_room="/create-a-room";
 
-    public static final String create_a_property="create/create-a-property";
+    public static final String create_a_property="/create-a-property";
     public static final String ERROR="error";
-    public static final String delete_a_room="/delete/delete-a-room/{id}";
+    public static final String delete_a_room="/delete-a-room/{id}";
 
-    public static final String get_all_room="/get/get-all-room";
+    public static final String get_all_room="/get-all-room";
 
-    public static final String get_a_room="/get/get-a-room/{id}";
+    public static final String get_a_room="/get-a-room/{id}";
 
-    public static final String edit_a_room="/edit/edit-a-room";
-    public static final String become_a_host="/host/become-a-host";
+    public static final String edit_a_room="/edit-a-room";
+    public static final String BECOME_A_HOST="/become-a-host";
 
+    public static final String SERVICE_NAME ="host" ;
 }
